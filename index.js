ec0aba0335fdf8eea72e408f39b6b84f82974789
kri-sh27 <hogalekrishna2000@gmail.com>
2023-01-03 10:53:21 +0530

    hello world

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express=require('express')
 const app=express()
+
+app.get('/',(request,response)=>{
+    return response.send("hello world")
+})
+
+app.listen(3000,()=>{
+    console.log("app runing on http://localhost:300")
+})
